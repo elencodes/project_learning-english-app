7cbf38db90507c19837e5756b33857bb38ef74c2
elencodes <playnonstop205@gmail.com>
2024-10-17 22:55:08 +0400

    added the ability to turn pages in the table

--- src/components/VocabularyPage/VocabularyPage.jsx
+++ src/components/VocabularyPage/VocabularyPage.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
 import { TableRow } from "../TableRow/TableRow";
 import data from "../../data/data.json";
 import styles from "./VocabularyPage.module.scss";
 
 export function VocabularyPage() {
+	// Управление состоянием текущей страницы и определяем количество строк на странице
+	const [currentPage, setCurrentPage] = useState(1);
+	const rowsPerPage = 5;
+
+	// Определение общего количества страниц
+	const totalPages = Math.ceil(data.length / rowsPerPage);
+
+	// Расчет индексов для отображаемых строк
+	const startIndex = (currentPage - 1) * rowsPerPage;
+	const endIndex = startIndex + rowsPerPage;
+	const currentRows = data.slice(startIndex, endIndex);
+
+	// Обработчик для перехода на следующую страницу
+	const handleNextPage = () => {
+		if (currentPage < totalPages) {
+			setCurrentPage(currentPage + 1);
+		}
+	};
+
+	// Обработчик для перехода на предыдущую страницу
+	const handlePrevPage = () => {
+		if (currentPage > 1) {
+			setCurrentPage(currentPage - 1);
+		}
+	};
+
 	return (
 		<>
 			<main className="container">
@@ -21,10 +48,10 @@ export function VocabularyPage() {
 					</thead>
 
 					<tbody className={styles.table__body}>
-						{data.map((props) => (
+						{currentRows.map((props, index) => (
 							<TableRow
 								key={props.id}
-								id={props.id}
+								id={startIndex + index + 1} // Отображаем номер строки с учетом страницы
 								theme={props.theme}
 								word={props.word}
 								transcription={props.transcription}
@@ -32,6 +59,45 @@ export function VocabularyPage() {
 							/>
 						))}
 					</tbody>
+					<tfoot className={styles.table__footer}>
+						<tr>
+							<th className={styles.table__footer_text}>
+								Total items:{" "}
+								<span className={styles.table__footer_counter}>
+									{data.length}
+								</span>
+							</th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th></th>
+							<th>
+								<div className={styles.pagination}>
+									<button
+										className={styles.paginationButton}
+										onClick={handlePrevPage}
+										disabled={currentPage === 1}
+									>
+										⮜
+									</button>
+									<span className={styles.pageInfo}>
+										{" "}
+										<span className={styles.pageInfo_currentPage}>
+											{currentPage}
+										</span>{" "}
+										of {totalPages}
+									</span>
+									<button
+										className={styles.paginationButton}
+										onClick={handleNextPage}
+										disabled={currentPage === totalPages}
+									>
+										⮞
+									</button>
+								</div>
+							</th>
+						</tr>
+					</tfoot>
 				</table>
 			</main>
 		</>
