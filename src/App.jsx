de32e74fda60cae773cee47b053b882f0e833c0e
elencodes <playnonstop205@gmail.com>
2024-11-23 17:49:18 +0400

    fixed Router

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,18 @@ import "./App.scss";
 function App() {
 	return (
 		<>
-			<Router basename="/project_learning-english-app">
+			<Router>
 				<div className="app">
 					<Header />
 					<Routes>
-						<Route path="/" element={<VocabularyPage />} />
-						<Route path="/game" element={<GamePage />} />
+						<Route
+							path="https://elencodes.github.io/project_learning-english-app/"
+							element={<VocabularyPage />}
+						/>
+						<Route
+							path="https://elencodes.github.io/project_learning-english-app/game"
+							element={<GamePage />}
+						/>
 						<Route path="*" element={<MissingPage />} />
 					</Routes>
 				</div>
