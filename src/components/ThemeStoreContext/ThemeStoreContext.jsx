a6ef3a73e6b9699839b60a52c89a24e46a89d00d
elencodes <playnonstop205@gmail.com>
2024-12-12 22:28:55 +0400

    A separate context has been created for ThemeStore.jsx

---
+++ src/components/ThemeStoreContext/ThemeStoreContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+import themeStore from "../../store/ThemeStore"; // импортируем экземпляр класса ThemeStore
+
+//Метод для создания нового контекста
+const ThemeStoreContext = createContext(null);
+
+export const ThemeStoreProvider = ({ children }) => {
+	return (
+		// Экземпляр класса кладем в провайдер, чтобы каждый компонент, который лежит внутри провайдера, имел доступ к глобальному хранилищу
+		<ThemeStoreProvider.Provider value={{ themeStore }}>
+			{children}
+		</ThemeStoreProvider.Provider>
+	);
+};
+
+export const useThemeStore = () => {
+	const context = useContext(ThemeStoreContext);
+	//проверка контекста на корректность
+	if (!context) {
+		throw new Error("useThemeStore must be used within a WordsStoreProvider");
+	}
+	return context;
+};
